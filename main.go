ed338f6d7b313a2f774e49331ed3a656a1f912d9
Rishabh Mishra <me@rsbh.dev>
2021-08-21 16:30:51 +0530

    chore: move models to seperate package

--- main.go
+++ main.go
@@ -6,25 +6,8 @@ import (
 
 	"github.com/casbin/casbin/v2"
 	"github.com/google/uuid"
+	"github.com/rsbh/auth/models"
 )
-
-type User struct {
-	Id   uuid.UUID
-	Name string
-}
-
-type Resource struct {
-	Id   uuid.UUID
-	Name string
-}
-
-func (u User) getId() string {
-	return u.Id.String()
-}
-
-func (r Resource) getId() string {
-	return r.Id.String()
-}
 
 func main() {
 	e, err := casbin.NewEnforcer("config/model.conf", "config/policy.csv")
@@ -32,14 +15,16 @@ func main() {
 		log.Fatalf("error: enforcer: %s", err)
 	}
 
-	user := User{uuid.New(), "Test User"}
-	resource := Resource{uuid.New(), "Test Resource"}
+	user := models.User{uuid.New(), "Test User"}
+	resource := models.Resource{uuid.New(), "Test Resource"}
+	group := models.Group{uuid.New(), "Test Group"}
 
 	// AddPolicy: Subject, Resource, Relationship, Action
-	e.AddPolicy(user.getId(), resource.getId(), "user:resource", "read")
+	e.AddPolicy(user.GetId(), resource.GetId(), models.GetRelation(user, resource), "read")
+	e.AddPolicy(group.GetId(), resource.GetId(), models.GetRelation(group, resource), "read")
 	e.SavePolicy()
 
-	ok, err := e.Enforce(user.getId(), resource.getId(), "user:resource", "read")
+	ok, err := e.Enforce(user.GetId(), resource.GetId(), "user:resource", "read")
 
 	if err != nil {
 		log.Fatalf("error: Enforce: %s", err)
---
+++ models/model.go
@@ -0,0 +1,54 @@
+package models
+
+import (
+	"fmt"
+
+	"github.com/google/uuid"
+)
+
+type User struct {
+	Id   uuid.UUID
+	Name string
+}
+
+type Resource struct {
+	Id   uuid.UUID
+	Name string
+}
+
+type Group struct {
+	Id   uuid.UUID
+	Name string
+}
+
+func (g Group) GetId() string {
+	return g.Id.String()
+}
+
+func (u User) GetId() string {
+	return u.Id.String()
+}
+
+func (r Resource) GetId() string {
+	return r.Id.String()
+}
+
+func getType(i interface{}) string {
+	switch i.(type) {
+	case User:
+		return "user"
+	case Resource:
+		return "resource"
+	case Group:
+		return "group"
+	default:
+		return ""
+	}
+}
+
+func GetRelation(i interface{}, j interface{}) string {
+	itype := getType(i)
+	jtype := getType(j)
+
+	return fmt.Sprintf("%s:%s", itype, jtype)
+}
